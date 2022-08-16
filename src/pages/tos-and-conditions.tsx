96f95a092b1f5af02a1cbc77080ae6cab2263e67
Ali Moallim <amoallim15@gmail.com>
2022-08-16 18:37:57 +0900

    margin update

--- src/pages/tos-and-conditions.tsx
+++ src/pages/tos-and-conditions.tsx
@@ -17,7 +17,7 @@ const ToSandConditions: NextPage = () => {
         </div>
         {/**/}
         <div
-          className="top-auto bottom-0 left-0 right-0 w-full absolute pointer-events-none overflow-hidden h-16"
+          className="top-auto bottom-0 left-0 right-0 w-full absolute pointer-events-none overflow-hidden h-20"
           style={{ transform: "translateZ(0)" }}
         >
           <svg
